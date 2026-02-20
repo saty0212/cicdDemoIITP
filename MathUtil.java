4c99b59d4103f6a502d347f06be8339fa1f24ed9
saty0212 <sschouhan.cse@mnit.ac.in>
2026-02-20 11:18:37 +0530

    initial files

---
+++ MathUtil.java
@@ -0,0 +1,8 @@
+public class MathUtil {
+    public static int add(int a, int b) {
+        return a+b;//bug fixed in add
+    }
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+}
---
+++ myApp.java
@@ -0,0 +1,6 @@
+public class myApp {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Sum: " + MathUtil.add(5, 10));
+    }
+}
